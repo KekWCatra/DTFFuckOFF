125a9771a07ed280176871a5528e51d0c280ef68
Kekw Catra <81526355+KekWCatra@users.noreply.github.com>
2021-08-03 07:34:46 +0300

    0.2

--- DTFFuckOFF.user.js
+++ DTFFuckOFF.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        DTF FuckOFF
 // @match       https://dtf.ru/*
-// @version     0.1  (2021-08-01)
+// @version     0.2  (2021-08-02)
 // @license     MIT
 // @author      KekW - https://dtf.ru/u/182912-kekw
 // @description В пизду DTF.
@@ -14,7 +14,7 @@ function someFuckingMagic()
     if (loc.toString().indexOf('.ru/u/') >= 0) {
         return;
     }
-    
+
     let commPinned = document.querySelectorAll('.comments__pinned');
     if (commPinned) {
         commPinned.forEach(function(commP) {
@@ -27,10 +27,19 @@ function someFuckingMagic()
     }
 }
 
+function goblinsDoTheirJob()
+{
+    document.querySelectorAll('.layout__right-column a:not([href*=".ru/u/"]):not([class=""])').forEach(function(commP) {
+        commP.parentNode.style.display = 'none';
+    });
+}
+
 addEventListener('DOMContentLoaded', function() {
     someFuckingMagic();
+    goblinsDoTheirJob();
 });
 
 addEventListener('DOMNodeInserted', function() {
     someFuckingMagic();
+    goblinsDoTheirJob();
 });
